3efea4bcdc5c8ac18b122f9c25002dca4a9b352a
jaesukpark77 <jaesukpark77@naver.com>
2021-04-26 01:14:14 +0900

    15552_빠른 A+B

---
+++ 15552_빠른 A+B/15552_빠른 A+B/15552_빠른 A+B.cpp
@@ -0,0 +1,19 @@
+﻿#include <cstdio>
+using namespace std;
+
+int main()
+{
+    int T;
+    int A, B;
+
+    scanf("%d", &T);
+
+    for (int i = 0; i < T; i++)
+    {
+        scanf("%d %d", &A, &B);
+        printf("%d\n", A+B);
+    }
+
+    return 0;
+}
+
