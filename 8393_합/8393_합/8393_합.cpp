2f96d4b18b80dfe5430124b9d573d8cfc6e52d7d
jaesukpark77 <jaesukpark77@naver.com>
2021-04-26 01:15:51 +0900

    8393_합

---
+++ 8393_합/8393_합/8393_합.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << n * (n + 1) / 2;
+
+
+	/*
+    int n, a=0;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		a = a + i;
+	}
+	cout << a;
+
+	return 0;
+	*/
+}
