29b42a401ad796d6a91d5a4340fd0704be0c39b9
jaesukpark77 <jaesukpark77@naver.com>
2021-04-26 15:16:25 +0900

    15552_빠른 A+B

--- 15552_빠른 A+B/15552_빠른 A+B/15552_빠른 A+B.cpp
+++ 15552_빠른 A+B/15552_빠른 A+B/15552_빠른 A+B.cpp
@@ -1,9 +1,11 @@
-﻿#include <cstdio>
+﻿#include <cstdio> //c방식 사용
+#include<iostream> //C++ 방식 사용
 using namespace std;
 
 int main()
 {
-    int T;
+    //printf,scanf 사용
+    /*int T;
     int A, B;
 
     scanf("%d", &T);
@@ -12,6 +14,24 @@ int main()
     {
         scanf("%d %d", &A, &B);
         printf("%d\n", A+B);
+    }
+
+    return 0;*/
+
+    //cout, cin 사용
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+
+    int T;
+    int A, B;
+
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        cin >> A >> B;
+        cout << A + B << "\n";
     }
 
     return 0;
